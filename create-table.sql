9cbc8ad65911ac1fbe1dd9732c8b8058d5581128
Dzemal Imamovic <dzemal.imamovic@icloud.com>
2020-09-26 16:20:14 +0200

    Initial Commit

---
+++ create-table.sql
@@ -0,0 +1,7 @@
+-- Benutze Datenbank 'seminar'
+USE seminar;
+-- Erstellen von Tabelle 'anwender'
+CREATE TABLE anwender
+(
+Vorname VARCHAR(30)
+);
---
+++ create.sql
@@ -0,0 +1,5 @@
+-- Datenbank anlegen
+DROP DATABASE IF EXISTS seminar;
+CREATE DATABASE seminar
+CHARACTER SET latin1
+COLLATE latin1_german2_ci;
