2a91e6de9b461afcd027e13e4939edc294686e07
Dzemal Imamovic <dzemal.imamovic@icloud.com>
2020-09-26 22:06:03 +0200

    Updates

--- create-table.sql
+++ create-table.sql
@@ -1,7 +1,10 @@
 -- Benutze Datenbank 'seminar'
 USE seminar;
 -- Erstellen von Tabelle 'anwender'
-CREATE TABLE anwender
-(
-Vorname VARCHAR(30)
+CREATE TABLE IF NOT EXISTS anwender (
+    Vorname VARCHAR(30)
 );
+
+INSERT INTO anwender(Vorname) VALUES('Huso');
+INSERT INTO anwender(Vorname) VALUES('Meho');
+INSERT INTO anwender(Vorname) VALUES('Andreas'),('Home'),('Hans');
